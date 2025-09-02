e39bf701a80c9cd4998af2a3029ecf230fbfc009
Nikhil <nikhilraikwar099@gmail.com>
2025-09-02 21:34:08 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import PortfolioReplica from './Portfolio'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="">
