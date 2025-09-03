ee517c567f4430387dd0217a8ada9cf72b9cfe38
Nikhil <nikhilraikwar099@gmail.com>
2025-09-03 16:26:25 +0530

    done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,13 +11,13 @@ function Skills({skillsref}) {
         </p>
       </div>
 
-      <div className="mt-10 flex flex-wrap  gap-y-10 gap-x-3 justify-evenly items-center ">
+      <div className="mt-10 flex flex-wrap text-slate-900 gap-y-10 gap-x-3 justify-evenly items-center ">
         <div className="w-full rounded-xl border  md:w-[70%] lg:w-[40%]  xl:w-[30%]  border-slate-200 bg-white px-6 py-10 text-center shadow-sm hover:shadow-md transition">
           <div className="mx-auto mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-gray-900 text-white">
             <FaUser className="w-5 h-5" />
           </div>
           <p className="font-semibold text-lg">Front-End Development</p>
-          <p className="mt-2 text-sm text-slate-600 leading-relaxed">
+          <p className="mt-2 text-sm leading-relaxed">
             Proficient in HTML, CSS and JavaScript. Experienced in responsive
             web design, building user-friendly interfaces, and using modern
             frameworks like ReactJS and NextJs.
@@ -28,7 +28,7 @@ function Skills({skillsref}) {
             <FaServer className="w-5 h-5" />
           </div>
           <p className="font-semibold text-lg">Back-End Development</p>
-          <p className="mt-2 text-sm text-slate-600 leading-relaxed">
+          <p className="mt-2 text-sm leading-relaxed">
             Skilled in server-side programming using technologies like Node.js
             and Express. Experience working with databases such as MongoDB and
             MySQL.
